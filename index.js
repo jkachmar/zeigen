ebbb6f19aac8fdb23131025cd8cdb35fcb01f027
Joe Kachmar <joseph.kachmar@gmail.com>
2017-03-02 21:22:30 -0500

    Adds auth check

--- index.js
+++ index.js
@@ -4,37 +4,56 @@ const Reader = require('fantasy-readers');
 
 /* -------------------------------------------------------------------------- */
 
-// :: Url ~ String  -- string representation of an HTTP URL
-// :: RequestBody ~ Object -- JSON object representing HTTP request body
-// :: Env ~ { url :: Url } -- Reader environment
+// :: AccessToken ~ String  -- globally mutable access token NOTE: UNSAFE
+// :: Env ~ Object          -- the Reader monad environment
+// :: FetchOptions ~ Object -- node-fetch method/request options
+// :: RequestBody ~ Object  -- HTTP request body
+// :: ResponseBody ~ Object -- HTTP response body
+// :: RefreshToken ~ String -- Authentication refresh token
+// :: Showable ~ String     -- Some type that can be stringified
+// :: Url ~ String          -- string representation of an HTTP URL
 
 /* -------------------------------------------------------------------------- */
 
-// :: Show a => a -> Error ()
+// :: AccessToken
+let accessToken = '';
+
+/* -------------------------------------------------------------------------- */
+
+// :: Showable -> Error ()
 const logError = x => console.error(x); // eslint-disable-line no-console
 
-// :: Show a => a -> Log ()
+// :: Showable -> Log ()
 const log = x => console.log(x); // eslint-disable-line no-console
 
 /* -------------------------------------------------------------------------- */
 
+// :: FetchOptions ->  -> Future Error Response
+const fetchf = opts => url =>
+      Future.fromPromise2(fetch, url, opts);
+
 // :: Url -> Future Error Response
-const getf = url => Future.fromPromise2(fetch, url, { method: 'GET' });
+const getf =
+      fetchf(
+        { method: 'GET',
+          headers: { Authorization: `Bearer ${accessToken}` },
+        });
 
 // :: RequestBody -> Url -> Future Error Response
-const postf = body => url =>
-      Future.fromPromise2(fetch, url,
+const postf = body =>
+      fetchf(
         { method: 'POST',
+          headers: { Authorization: `Bearer ${accessToken}` },
           body: JSON.stringify(body),
         });
 
-// :: Response -> ResponseBody
+// :: Response -> Future Error ResponseBody
 const resolveJson = res => Future.fromPromise(() => res.json(), 0);
 
 /* -------------------------------------------------------------------------- */
 
 // :: Array (Future a b) -> Future a (Array b)
-const sequence = Future.parallel(5);
+const sequence = Future.parallel(Infinity);
 
 // :: Array (Future Error Response) -> Future Error (Array Response)
 const sequenceGets = urls => sequence(urls.map(getf));
@@ -45,24 +64,46 @@ const sequencePosts = body => urls => sequence(urls.map(postf(body)));
 // :: ResponseBody -> Array (Future Error Response) -> Future Error (Array ResponseBody)
 const sequenceRespBodies = resps => sequence(resps.map(resolveJson));
 
+// NOTE: This function is side effecting!
+// :: Url -> RefreshToken -> Response -> Future Error Response
+const unsafeCheckAuth = auth => token => (resp) => {
+  if (resp.status === 403) {
+    return (postf({ refresh_token: token })(auth)
+            .chain(resolveJson)
+            .map((body) => {
+              accessToken = JSON.parse(body.data).refresh_token; // NOTE: change this to 'token'
+              return body;
+            }));
+  } else if (resp.status >= 400) {
+    return Future.reject(resp);
+  }
+  return Future.of(resp);
+};
+
 /* -------------------------------------------------------------------------- */
 
-// The main function consists of a 'Reader' monad containing the app config
+// The application logic can be thought of as a series of pure and effectful
+// computations over an 'Env'ironment accessed from a 'Reader' monad
 // :: Reader Env
 const main = () =>
       Reader(env =>
-             sequencePosts({ test: 'test' })(env.urls)
+             getf(env.teams)
+             .chain(unsafeCheckAuth(env.auth)(env.token))
+             .chain(() => sequenceGets(env.urls))
              .chain(sequenceRespBodies)
-             .map(rs => rs.map(r => r.data)));
+             .map(rs => rs.map(r => r.url)));
 
 /* -------------------------------------------------------------------------- */
 
 if (require.main === module) {
   main().run({
-    urls:
-    [
-      'https://httpbin.org/post',
-      'https://httpbin.org/post',
+    teams: 'https://httpbin.org/status/403',
+    token: 'test',
+    auth: 'https://httpbin.org/post',
+    urls: [
+      'https://httpbin.org/get',
+      'https://httpbin.org/get',
+      'https://httpbin.org/get',
     ],
   }).fork(log, logError);
 }
